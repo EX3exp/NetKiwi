121fb9d3bf4aa7a5d52fe095292cb0e444fa23e6
EX3 <inthe_6788@naver.com>
2024-10-18 10:57:16 +0900

    Fixed linux dll path

--- NetKiwi/NetKiwi/KiwiCAPILinux.cs
+++ NetKiwi/NetKiwi/KiwiCAPILinux.cs
@@ -21,7 +21,7 @@ namespace NetKiwi.Backend
     public class KiwiCAPILinux: KiwiCAPIBase
     {
 
-        private const string dll_name = "libkiwi.so";
+        private const string dll_name = "runtimes/linux-x86-x64/lib/libkiwi.so";
 
 
        

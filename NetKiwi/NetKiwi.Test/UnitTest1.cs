8116c0f3e0b6918492213a879226a1f84b3848da
EX3 <inthe_6788@naver.com>
2024-12-28 10:53:21 +0900

    Fixed test, Fixed typo in .targets

--- NetKiwi/NetKiwi.Test/UnitTest1.cs
+++ NetKiwi/NetKiwi.Test/UnitTest1.cs
@@ -68,7 +68,7 @@ namespace test
         [TestMethod]
         public void TestExtractAddWords()
         {
-            SharpKiwi sharpKiwi = new SharpKiwi("C:/Users/inthe/Downloads/NetKiwi/NetKiwi/NetKiwi/netkiwi/models");
+            SharpKiwi sharpKiwi = new SharpKiwi();
             string[] arr = new string[100];
             for (int i = 0; i < 100; ++i)
             {
